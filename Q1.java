608ca998e5b8022eac69448d2d0cd11256be1361
Rajat Saini <rajat.saini2004@gmail.com>
2026-08-16 13:00:40 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,23 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+        
+        String studentName;
+        int rollNumber;
+        double mathMarks;
+        double scienceMarks;
+        double englishMarks;
+
+        studentName = "Rajat Saini";
+        rollNumber = 37;
+        mathMarks = 88.5;
+        scienceMarks = 92.0;
+        englishMarks = 85.5;
+
+        System.out.println("--- Student Details ---");
+        System.out.println("Name: " + studentName);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Math: " + mathMarks);
+        System.out.println("Science: " + scienceMarks);
+        System.out.println("English: " + englishMarks);
+    }
+}
