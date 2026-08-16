f1c7ff71a1ae841365f7d021215dffa4600f45fa
Rajat Saini <rajat.saini2004@gmail.com>
2026-08-16 13:28:05 +0530

    Add files via upload

---
+++ Q6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number for its multiplication table: ");
+        int number = scanner.nextInt();
+        
+        System.out.println("\n--- Multiplication Table for " + number + " ---");
+        
+        for (int i = 1; i <= 10; i++) {
+            
+            int product = number * i;
+            
+            System.out.println(number + " x " + i + " = " + product);
+        }
+        
+        scanner.close();
+    }
+}
